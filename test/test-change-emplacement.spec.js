bf1622e16708f2660585422a633f46c13f8407a6
Azzler1 <106780237+Azzler1@users.noreply.github.com>
2024-11-04 13:46:13 +0100

    Update test-change-emplacement.spec.js

--- test/test-change-emplacement.spec.js
+++ test/test-change-emplacement.spec.js
@@ -14,16 +14,12 @@ test.describe('Mise à jour de l\'emplacement', () => {
     // Cliquer sur l'icône d'emplacement
     await homePage.clickLocationIcon();
 
-    // Saisir le nouveau code postal
+    // Saisir le nouveau code postalll
     await homePage.enterPostalCode('75001');
 
-    // Valider la mise à jour
+    // Validerr la mise à jour
     await homePage.submitLocation();
-
-    // Re-cliquer sur l'icône d'emplacement pour vérifier la mise à jour
     await homePage.clickLocationIcon();
-
-    // Vérifier que l'input contient le code postal 75001
     const locationInputValue = await page.inputValue('input#GLUXZipUpdateInput');
     expect(locationInputValue).toBe('75001');
   });
