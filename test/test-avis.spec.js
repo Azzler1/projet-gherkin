bf4de84ad81492378c922882dc7aed773a8b1007
Azzler1 <106780237+Azzler1@users.noreply.github.com>
2024-11-04 13:45:36 +0100

    Update test-avis.spec.js

--- test/test-avis.spec.js
+++ test/test-avis.spec.js
@@ -3,13 +3,10 @@
 import { test, expect } from '@playwright/test';
 
 test('Vérifier l\'affichage des avis clients pour un produit', async ({ page }) => {
-    // Étape 1 : Aller à la page du produit
     await page.goto('https://www.amazon.fr/Simplissime-light-livre-cuisine-facile/dp/2011356423');
 
-    // Étape 2 : Cliquer sur le lien de la section "Avis clients"
     await page.click('#acrCustomerReviewLink');
 
-    // Étape 3 : Vérifier que les avis clients sont affichés
     const reviewsSection = await page.isVisible('#customerReviews');
     expect(reviewsSection).toBeTruthy();
 });
