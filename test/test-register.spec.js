acf1f88ebf35dc7e8d8a381c3590ccab09b24170
Azzler1 <106780237+Azzler1@users.noreply.github.com>
2024-11-04 13:47:11 +0100

    Update test-register.spec.js

--- test/test-register.spec.js
+++ test/test-register.spec.js
@@ -2,13 +2,12 @@
 // TEST AVEC EMAIL VALIDE
 const { test, expect } = require('@playwright/test');
 const RegisterPage = require('../pages/registerPage'); 
-// modifier le chemin si executé sur un autre pc que le mien
 
 test.describe('Création de compte', () => {
   let registerPage;
 
   test.beforeEach(async ({ page }) => {
-    registerPage = new RegisterPage(page);   //url de registering d'amazon 
+    registerPage = new RegisterPage(page); 
     await page.goto('https://www.amazon.fr/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.fr%2F%3F_encoding%3DUTF8%26ref_%3Dnav_em_hd_re_signin&prevRID=3Z3MCB47BSTABAN0MDW5&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=frflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
   });
 
